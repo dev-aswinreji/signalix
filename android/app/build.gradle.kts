fd5fedf583e1f89ad6bf0d3ad14bb9fd8a70a430
Aswin Reji <aswinreji.004@gmail.com>
2026-03-16 19:30:09 +0000

    chore: bump version 1.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.signalix.app"
         minSdk = 35
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
     }
 
     buildTypes {
