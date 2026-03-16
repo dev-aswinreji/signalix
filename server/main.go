5cd07bd57d24c492ff2e813f855cc14c5ed421f0
Aswin Reji <aswinreji.004@gmail.com>
2026-03-16 18:20:00 +0000

    feat: go auth register/login with uuid token

--- server/main.go
+++ server/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type User struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
 
 type RegisterReq struct {
 	Username string `json:"username"`
@@ -12,6 +20,22 @@ type RegisterReq struct {
 
 type RegisterRes struct {
 	Token string `json:"token"`
+}
+
+type LoginReq struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
+var (
+	usersMu sync.Mutex
+	users   = make(map[string]User)
+)
+
+func newToken() string {
+	b := make([]byte, 16)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
 }
 
 func main() {
@@ -29,15 +53,34 @@ func main() {
 		}
 		var req RegisterReq
 		_ = json.NewDecoder(r.Body).Decode(&req)
-		// TODO: generate UUID, store user, return token
-		res := RegisterRes{Token: "uuid-placeholder"}
+		if req.Username == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		usersMu.Lock()
+		defer usersMu.Unlock()
+		if _, exists := users[req.Username]; exists {
+			w.WriteHeader(http.StatusConflict)
+			return
+		}
+		token := newToken()
+		users[req.Username] = User{Username: req.Username, Token: token}
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(res)
+		json.NewEncoder(w).Encode(RegisterRes{Token: token})
 	})
 
 	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		var req LoginReq
+		_ = json.NewDecoder(r.Body).Decode(&req)
+		usersMu.Lock()
+		defer usersMu.Unlock()
+		user, ok := users[req.Username]
+		if !ok || user.Token != req.Token {
+			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 		w.WriteHeader(http.StatusOK)
