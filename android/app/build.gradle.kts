4e5882c4656fa0c3dc906d957191501539ba9920
Aswin Reji <aswinreji.004@gmail.com>
2026-03-17 05:26:07 +0000

    feat: icon-only copy button

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.signalix.app"
         minSdk = 35
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.2.1"
+        versionCode = 9
+        versionName = "1.2.2"
     }
 
     buildTypes {
